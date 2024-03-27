ae86ad6a77d8abc9452c322184657c663552bef7
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-03-27 22:10:59 +0530

    -added delete user review endpoints for admin and user

--- controllers/adminRouter.js
+++ controllers/adminRouter.js
@@ -73,4 +73,15 @@ adminRouter.get('/reviews',(_req,res)=>{
   })
   res.status(200).json(reviews)
 })
+
+adminRouter.delete('/reviews/:id',(req,res)=>{
+  const id = Number(req.params.id)
+  const remove = Number(req.body.review_id)
+  const business = businessData.businessData.find(i => Number(i.id)===id)
+  const reviews = business.reviews.filter(i=>Number(i.user_id)!==remove)
+  business.reviews = reviews
+  businessData.businessData = businessData.businessData.filter(business => Number(business.id) !== id)
+  businessData.businessData = [...businessData.businessData,business]
+  res.sendStatus(204)
+})
 module.exports = adminRouter
