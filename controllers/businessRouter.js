66f521e5449322d4dc532fa99edeeddfb3a7ef37
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-03-27 11:39:48 +0530

    -business backend now can view individual businesses and minor refactor

--- controllers/businessRouter.js
+++ controllers/businessRouter.js
@@ -1,12 +1,12 @@
 const businessRouter = require('express').Router()
-let data = [
-  {
-    name: "test2",
-    information: "information2",
-  },
-];
+const businessData = require('../data/businessData')
 businessRouter.get('/',async (_req,res) => {
-    res.json(data)
+    res.status(200).json(businessData.businessData)
+})
+businessRouter.get('/:id',async(req,res)=>{
+  const id = req.params.id;
+  const business = await businessData.businessData.find( i => i.id === id)
+  res.status(200).json(business);
 })
 
 module.exports = businessRouter
---
+++ data/businessData.js
@@ -0,0 +1,28 @@
+let businessData = [
+  {
+    'id': '001',
+    'nameOfBusiness': 'Banand Ahavan',
+    'phone' : '1234567890',
+    'city': 'Bangalore',
+    'address': 'Banand Ahavan - 01, Bangalore - 560094',
+    'images': 'https://google.com'
+  },
+  {
+    'id': '002',
+    'nameOfBusiness': 'Chinese Corner',
+    'phone' : '1234567891',
+    'city': 'Bangalore',
+    'address': 'Chinese Corner - 01, Bangalore - 560094',
+    'images': 'https://google.com'
+  },
+  {
+    'id': '003',
+    'nameOfBusiness': 'Dravid\'s Dosa Domain',
+    'phone' : '1234567892',
+    'city': 'Bangalore',
+    'address': 'Dravid\'s Dosa Domain - 01, Bangalore - 560094',
+    'images': 'https://google.com'
+  },
+]
+
+module.exports = {businessData}
